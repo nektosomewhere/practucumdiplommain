7bc01c124cb1357322f8a1d6e7328b9b885684eb
nektosomewhere <alexeypogodin17@gmail.com>
2024-10-19 19:58:17 -0400

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,8 @@
+# URL_SERVICE хранит базовый URL веб-сервиса, который используется для доступа к API или другим ресурсам.
+URL_SERVICE = "https://cf52d102-7e13-454b-9868-26cbf5076295.serverhub.praktikum-services.ru/"
+
+# CREATE_ORDER хранит путь к API-методу для создания нового заказа.
+CREATE_ORDER = "/api/v1/orders"
+
+# PUT_ORDER хранит путь к API-методу для создания нового набора.
+PUT_ORDER = "/api/v1/orders/track"
