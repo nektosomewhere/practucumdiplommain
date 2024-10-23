724fe84e4454e78ce04fb947608d38f8ee5a1933
nektosomewhere <alexeypogodin17@gmail.com>
2024-10-22 21:23:10 -0400

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,8 +1,8 @@
 # URL_SERVICE хранит базовый URL веб-сервиса, который используется для доступа к API или другим ресурсам.
-URL_SERVICE = "https://cf52d102-7e13-454b-9868-26cbf5076295.serverhub.praktikum-services.ru/"
+URL_SERVICE = "https://0ada0b2d-b955-444d-9d70-8f47abcb82ff.serverhub.praktikum-services.ru"
 
 # CREATE_ORDER хранит путь к API-методу для создания нового заказа.
 CREATE_ORDER = "/api/v1/orders"
 
-# PUT_ORDER хранит путь к API-методу для создания нового набора.
+# PUT_ORDER хранит путь к API-методу для получения заказа по трекеру.
 PUT_ORDER = "/api/v1/orders/track"
